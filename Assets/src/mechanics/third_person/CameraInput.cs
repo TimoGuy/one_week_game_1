e25fd3761967b49ba70ff2e03b389142565a930b
Hawsoo <Hawsoo@users.noreply.github.com>
2019-10-17 20:32:18 -0700

    Adds third_person camera and basic movement

---
+++ Assets/src/mechanics/third_person/CameraInput.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// https://answers.unity.com/questions/395369/3rd-person-controller-1.html
+public class CameraInput : MonoBehaviour {
+	public Transform targetLookAt;
+	
+	public float distance = 5;
+	public float minDistance = 3;
+	public float maxDistance = 10;
+
+	public float xMouseSensitivity = 5;
+	public float yMouseSensitivity = 5;
+	public float mouseWheelSensitivity = 5;
+	public float yMinLimit = -40;
+	public float yMaxLimit = 80; 
+
+
+	public float mouseX, mouseY = 5;
+
+	// Use this for initialization
+	void Start () {
+		distance = Mathf.Clamp(distance, minDistance, maxDistance);
+		Reset();
+
+		mouseX = 0;
+		mouseY = 5;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		if (targetLookAt == null)
+        	return;
+
+		HandlePlayerInput();
+			
+		Vector3 newPos = CalculateDesiredPosition(mouseX, mouseY);
+			
+		UpdatePosition(newPos);
+	}
+
+	private void Reset () {
+
+	}
+
+	private void HandlePlayerInput () {
+		mouseX += Input.GetAxis("Mouse X") * xMouseSensitivity;
+        // mouseY -= Input.GetAxis("Mouse Y") * yMouseSensitivity;
+
+		mouseY = ClampAngle(mouseY, yMinLimit, yMaxLimit);
+	}
+
+	private Vector3 CalculateDesiredPosition (float rotX, float rotY) {
+		Vector3 dir = new Vector3(0, 0, -distance);
+		Quaternion rot = Quaternion.Euler(rotY, rotX, 0);
+		return targetLookAt.position + (rot * dir);
+	}
+
+	private void UpdatePosition (Vector3 newPos) {
+		transform.position = newPos;
+		transform.LookAt(targetLookAt);
+	}
+
+	float ClampAngle (float angle, float min, float max) {
+	while (angle < -360 || angle > 360) {
+		if (angle < -360) angle += 360;
+		if (angle > 360) angle -= 360;
+	}
+
+    return Mathf.Clamp(angle, min, max);
+ }
+}
---
+++ Assets/src/mechanics/third_person/ThirdPersonControllerInput.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonControllerInput : MonoBehaviour {
+	public Transform myCamera;
+	public float mvtSpeed = 5;
+	public float mvtAccel = 1;
+	public float gravityConst = 0.5f;
+
+	private CharacterController characterController;
+
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent<CharacterController>();
+		gravityBuildup = 0;
+		mvtBuildup = 0;
+	}
+	
+	private float mvtBuildup;
+	private Vector2 normalizedInputVec;
+	void FixedUpdate () {
+		float inputX = Input.GetAxisRaw("Horizontal");
+		float inputY = Input.GetAxisRaw("Vertical");
+		Vector2 myNormInputVec = new Vector2(inputX, inputY).normalized;
+		
+		if (Mathf.Abs(myNormInputVec.x) + Mathf.Abs(myNormInputVec.y) > 0) {
+			mvtBuildup += mvtAccel;
+			normalizedInputVec = myNormInputVec;
+		} else {
+			mvtBuildup -= mvtAccel;
+		}
+		mvtBuildup = Mathf.Clamp(mvtBuildup, 0, mvtSpeed);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveVector =
+			(myCamera.forward * normalizedInputVec.y + myCamera.right * normalizedInputVec.x).normalized *
+			mvtBuildup *
+			Time.deltaTime;
+		moveVector = new Vector3(moveVector.x, 0, moveVector.z);
+		UpdateGravity(ref moveVector);
+
+		characterController.Move(moveVector);
+	}
+
+	private float gravityBuildup;
+	private void UpdateGravity (ref Vector3 moveVector) {
+		if (characterController.isGrounded) {
+			gravityBuildup = 0;
+		} else {
+			gravityBuildup += gravityConst * Time.deltaTime;
+		}
+		
+		moveVector.y -= gravityBuildup;
+	}
+}
