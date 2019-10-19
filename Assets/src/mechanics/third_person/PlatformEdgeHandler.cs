453c1286160e0002a9975baa0358cfc676941cce
Hawsoo <Hawsoo@users.noreply.github.com>
2019-10-19 14:37:36 -0700

    Fixes Raycast and makes player stick to the ground while moving.

--- Assets/src/mechanics/third_person/PlatformEdgeHandler.cs
+++ Assets/src/mechanics/third_person/PlatformEdgeHandler.cs
@@ -27,25 +27,31 @@ public class PlatformEdgeHandler : MonoBehaviour {
 		return new Vector2(transform.position.x, transform.position.z);
 	}
 
-	private bool IsOnGround () {
+	public bool IsOnGround (out RaycastHit hitInfo) {
 		Ray ray = new Ray();
 		ray.origin = transform.position + Vector3.down;
 		ray.direction = Vector3.down;
 		Debug.DrawRay(ray.origin, ray.direction, Color.red);
 		return Physics.Raycast(
 			ray,
-			1,
-			LayerMask.NameToLayer("Ground")
+			out hitInfo,
+			0.5f,
+			1 << LayerMask.NameToLayer("Ground")
 		);
+	}
+
+	public bool IsOnGround () {
+		RaycastHit garb;
+		return IsOnGround(out garb);
 	}
 
 	private bool prevIsGrounded;
 	private void UpdateJump () {
-		if (!characterController.isGrounded &&
+		if (!IsOnGround() &&
 			prevIsGrounded) {
 			SendMessage("RequestJump");
 		}
 
-		prevIsGrounded = characterController.isGrounded;
+		prevIsGrounded = IsOnGround();
 	}
 }
--- Assets/src/mechanics/third_person/ThirdPersonControllerInput.cs
+++ Assets/src/mechanics/third_person/ThirdPersonControllerInput.cs
@@ -9,18 +9,20 @@ public class ThirdPersonControllerInput : MonoBehaviour {
 	public float jumpHeight = 10;
 
 	private CharacterController characterController;
+	private PlatformEdgeHandler platformEdgeHandler;
 
 
 	// Use this for initialization
 	void Start () {
 		characterController = GetComponent<CharacterController>();
+		platformEdgeHandler = GetComponent<PlatformEdgeHandler>();
 		mvtBuildup = 0;
 	}
 	
 	private float mvtBuildup;
 	private Vector2 normalizedInputVec;
 	void FixedUpdate () {
-		if (!characterController.isGrounded) return;
+		if (!platformEdgeHandler.IsOnGround()) return;
 		float inputX = Input.GetAxisRaw("Horizontal");
 		float inputY = Input.GetAxisRaw("Vertical");
 		Vector2 myNormInputVec = new Vector2(inputX, inputY).normalized;
@@ -35,30 +37,52 @@ public class ThirdPersonControllerInput : MonoBehaviour {
 	}
 
 	private Vector3 moveVector;
-
 	// Update is called once per frame
 	void Update () {
-		float prevY = moveVector.y;
 		moveVector =
 			(GetFlattenedForwardCamera() * normalizedInputVec.y
 				+ myCamera.right * normalizedInputVec.x)
 				.normalized
 			* mvtBuildup;
-		moveVector = new Vector3(moveVector.x, prevY, moveVector.z);
-		UpdateGravity();
+		moveVector = new Vector3(moveVector.x, FetchYVelo(), moveVector.z);
 
 		characterController.Move(moveVector * Time.deltaTime);
 	}
 
+	bool _reqJump = false;
 	public void RequestJump () {
-		moveVector.y = jumpHeight;
+		_reqJump = true;
 	}
 
 	private Vector3 GetFlattenedForwardCamera () {
 		return new Vector3(myCamera.forward.x, 0, myCamera.forward.z).normalized;
 	}
 
-	private void UpdateGravity () {
-		moveVector.y -= gravityConst * Time.deltaTime;
+	private bool _isJumping = false;
+	private float FetchYVelo () {
+		float yVelo = characterController.velocity.y;
+		if (_isJumping) {
+			if (yVelo >= 0 && platformEdgeHandler.IsOnGround()) {
+				_isJumping = false;
+			}
+		} else {
+			// Prevent flying up ramp
+			// and flying off going down
+			RaycastHit hitInfo;
+			platformEdgeHandler.IsOnGround(out hitInfo);
+			yVelo = Mathf.Min(
+				yVelo,
+				-Mathf.Max(hitInfo.distance / Time.deltaTime, 0)	// Division by time to counter later multiplication by time to force down distance
+			);
+		}
+
+		yVelo -= gravityConst * Time.deltaTime;
+		if (_reqJump) {
+			yVelo = jumpHeight;
+			_isJumping = true;
+			_reqJump = false;
+		}
+
+		return yVelo;
 	}
 }
