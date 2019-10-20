42bf41dd3e9bc00cb696e9024f90c6d4590764c8
Hawsoo <Hawsoo@users.noreply.github.com>
2019-10-19 22:20:59 -0700

    Adds attack animation and attaches triggers to WSA.cs

--- Assets/src/mechanics/weapon/WeaponSwordAttack.cs
+++ Assets/src/mechanics/weapon/WeaponSwordAttack.cs
@@ -2,14 +2,35 @@
 using System.Collections;
 
 public class WeaponSwordAttack : MonoBehaviour {
+	public CharacterController player;
+	public Transform weaponSword;
+	public string attackVariantType = "none";
+
+	private Animator weaponSword_animator;
 
 	// Use this for initialization
 	void Start () {
-	
+		weaponSword_animator = weaponSword.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (CheckWantToAttack()) {
+			UpdateAttackVariant();
+			Attack();
+		}
+	}
+
+	private bool CheckWantToAttack () {
+		return Input.GetButtonDown("Fire1");
+	}
+
+	private void UpdateAttackVariant () {
+
+	}
+
+	private void Attack () {
+		Debug.Log("Attack!!!");
+		weaponSword_animator.SetTrigger("Attack");
 	}
 }
