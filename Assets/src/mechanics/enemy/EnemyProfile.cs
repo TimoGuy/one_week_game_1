6cc78d5d2a7363cde7fc160f917b6dcc4e38e040
Hawsoo <Hawsoo@users.noreply.github.com>
2019-10-22 18:53:16 -0700

    Creates the enemy profile

---
+++ Assets/src/mechanics/enemy/EnemyProfile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyProfile : MonoBehaviour {
+	public int attackPoints;
+	public string triggerCodeFunction;
+	public MonoBehaviour triggerCode;
+	private Transform player;
+
+	// Use this for initialization
+	void Start () {
+		FindPlayer();
+	}
+
+	public void OnDie () {
+		if (triggerCode != null &&
+			triggerCodeFunction.Trim().Length > 0) {
+			triggerCode.SendMessage(triggerCodeFunction.Trim());
+		} 
+	}
+
+	public Transform GetPlayerTransform () {
+		return player;
+	}
+
+	private void FindPlayer () {
+		player = GameObject.FindWithTag("Player").transform;
+	}
+}
