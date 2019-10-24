4ccdbad8464f1af557a9733ffba279b2e6d5c21d
Hawsoo <Hawsoo@users.noreply.github.com>
2019-10-24 07:27:53 -0700

    Creates enemy animation and a placement-updator to persist location of animations

--- Assets/src/mechanics/enemy/EnemyProfile.cs
+++ Assets/src/mechanics/enemy/EnemyProfile.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class EnemyProfile : MonoBehaviour {
 	public int attackPoints;
-	public string triggerCodeFunction;
+	public Transform innerEnemyTrans;
+	private Vector3 _ietPosCopy = Vector3.zero;
+	public string triggerCodeFunction = "OnEnemyDie";
 	public MonoBehaviour triggerCode;
 	private Transform player;
 
 	// Use this for initialization
 	void Start () {
 		FindPlayer();
+		SaveInnerTransform();
 	}
 
 	public void OnDie () {
@@ -25,5 +28,25 @@ public class EnemyProfile : MonoBehaviour {
 
 	private void FindPlayer () {
 		player = GameObject.FindWithTag("Player").transform;
+	}
+
+	private void SaveInnerTransform () {
+		_ietPosCopy = innerEnemyTrans.localPosition;
+		// _ietPosCopy.rotation = innerEnemyTrans.rotation;
+		// _ietPosCopy.localScale = innerEnemyTrans.localScale;
+	}
+
+	public void MoveToInnerTransAsOffset () {
+		Debug.Log("Hey my duuude?!");
+		Debug.Log(innerEnemyTrans.position - _ietPosCopy);
+		transform.localPosition +=
+			innerEnemyTrans.localPosition - _ietPosCopy;
+		// transform.rotation +=
+		// 	innerEnemyTrans.rotation - _ietPosCopy.rotation;
+		// transform.localScale +=
+		// 	innerEnemyTrans.localScale - _ietPosCopy.localScale;
+
+		// Reset position of inner
+		innerEnemyTrans.localPosition = _ietPosCopy;
 	}
 }
