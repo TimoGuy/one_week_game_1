b6382cc1fc69c433d56d99c599c8866679a0d57d
Hawsoo <Hawsoo@users.noreply.github.com>
2019-12-08 21:31:51 -0800

    Adds Fight sequence mock playtesting thing

---
+++ Assets/TestAnimHelper.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAnimHelper : MonoBehaviour {
+	void CalledByAnimator_Rotate45 () {
+		var ang = transform.rotation.eulerAngles;
+		ang.y += 45;
+		transform.rotation = Quaternion.Euler(ang);
+	}
+}
