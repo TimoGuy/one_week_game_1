7a736b520f96e53bd67640c71146b305270433f7
Hawsoo <Hawsoo@users.noreply.github.com>
2019-12-10 00:05:12 -0800

    Plays with hurt sounds and stomp sounds

---
+++ Assets/src/mechanics/weapon/AttackReceiver.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackReceiver : MonoBehaviour {
+	public string ReceivingTag = "Untagged";
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == ReceivingTag) {
+			Debug.Log("Attacked!!!");
+			GetComponent<AudioSource>().Play(0);
+		}
+	}
+}
