8d5d52b1472393a3b56543fe38f7f4917e4e6d94
Hawsoo <Hawsoo@users.noreply.github.com>
2019-12-14 15:05:31 -0800

    Tunes and adds sfx

---
+++ Assets/src/SFXPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SFXPlayer : MonoBehaviour {
+	public AudioSource audioSource;
+	public AudioClip acPowerDown, acJump, acFall, acSlam;
+
+	void PlayPowerDown () { audioSource.PlayOneShot(acPowerDown); }
+	void PlayJump () { audioSource.PlayOneShot(acJump); }
+	void PlayFall () { audioSource.PlayOneShot(acFall); }
+	void PlaySlam () { audioSource.PlayOneShot(acSlam); }
+}
