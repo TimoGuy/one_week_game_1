b86b38b7451871138d5394c0eb358a98dd457fd5
Hawsoo <Hawsoo@users.noreply.github.com>
2019-12-14 20:28:25 -0800

    Starts creating phase 2

---
+++ Assets/src/mechanics/enemy/BossFightHandler.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossFightHandler : MonoBehaviour {
+	public GameObject phase2Container;
+	public GameObject phase2BubbleScoresIndicator;
+}
