fe97df4409c6b4d02a48f93311e25c942cd4cd7f
Hawsoo <Hawsoo@users.noreply.github.com>
2019-12-14 23:21:53 -0800

    Fixes some bugs of being able to slash the barrier early

--- Assets/src/mechanics/enemy/BossFightBubbleScores.cs
+++ Assets/src/mechanics/enemy/BossFightBubbleScores.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class BossFightBubbleScores : MonoBehaviour {
 	public int numTimesStompBeforeCrack = 5;
-	private int numTimesStomped = 0;
+	private int numTimesStomped;
 	private MeshRenderer meshRenderer;
 	private SphereCollider myCollider;
 	private bool amIVertical;
 
 	// Use this for initialization
-	void Start () {
+	void OnEnable () {
 		meshRenderer = GetComponent<MeshRenderer>();
 		myCollider = GetComponent<SphereCollider>();
 		myCollider.enabled = meshRenderer.enabled = false;
+		numTimesStomped = 0;
 	}
 
 	void OnTriggerEnter (Collider other) {
