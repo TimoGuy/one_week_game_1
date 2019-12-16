49c689a3901936752d79c333522ca41345f5af77
Hawsoo <Hawsoo@users.noreply.github.com>
2019-12-15 16:20:21 -0800

    Revert "POC: lazer follows player. Hmmm"
    This reverts commit 5ba1a7fbf43c32b75cf317f3612dea3dc84c9159.

--- Assets/TestAnimHelper.cs
+++ Assets/TestAnimHelper.cs
@@ -2,25 +2,9 @@
 using System.Collections;
 
 public class TestAnimHelper : MonoBehaviour {
-	public TestAnimHelper child;
-
 	void CalledByAnimator_Rotate45 () {
 		var ang = transform.rotation.eulerAngles;
 		ang.y += 45;
 		transform.rotation = Quaternion.Euler(ang);
-	}
-
-	void CalledByAnimator_Rotate45Child () {
-		child.SendMessage("CalledByAnimator_Rotate45", SendMessageOptions.RequireReceiver);
-	}
-
-	void CalledByAnimator_Rotate45Oppo () {
-		var ang = transform.rotation.eulerAngles;
-		ang.y -= 45;
-		transform.rotation = Quaternion.Euler(ang);
-	}
-
-	void CalledByAnimator_Rotate45OppoChild () {
-		child.SendMessage("CalledByAnimator_Rotate45Oppo", SendMessageOptions.RequireReceiver);
 	}
 }
--- Assets/src/mechanics/enemy/BossFightHandler.cs
+++ Assets/src/mechanics/enemy/BossFightHandler.cs
@@ -4,8 +4,6 @@ using System.Collections;
 public class BossFightHandler : MonoBehaviour {
 	public GameObject phase2;
 	public BossFightBubbleScores bubbleScores;
-	public Transform progLazerContainer;
-	public Transform playerCollision;
 	private Animator animator;
 
 	void Start () {
@@ -21,33 +19,8 @@ public class BossFightHandler : MonoBehaviour {
 		bubbleScores.SendMessage("StompedReceiveShock");
 	}
 
-	void Update () {
-		RandomChooseToDoLazerOrStomp();
-	}
-
 	void RandomChooseToDoLazerOrStomp () {
-		if (!animator.GetBool("Override_DoLazerBeam")) {
-			animator.SetBool("DoLazerBeam", Random.value > 0.5f);
-		}
-	}
-
-	void CheckLazerTurnLeftOrRight () {
-		float playerAngFromOrigin = Mathf.Atan2(playerCollision.position.z, playerCollision.position.x) * Mathf.Rad2Deg;
-		float diff = Mathf.DeltaAngle(
-			-playerAngFromOrigin, progLazerContainer.rotation.eulerAngles.y
-		);
-		Debug.Log(diff);
-		if (diff < 0) {
-			animator.SetFloat("LazerSpinRight", 0);
-			var ang = progLazerContainer.rotation.eulerAngles;
-			ang.y += 45;
-			progLazerContainer.rotation = Quaternion.Euler(ang);
-		} else {
-			animator.SetFloat("LazerSpinRight", 1);
-			var ang = progLazerContainer.rotation.eulerAngles;
-			ang.y -= 45;
-			progLazerContainer.rotation = Quaternion.Euler(ang);
-		}
+		animator.SetBool("DoLazerBeam", Random.value > 0.5f);
 	}
 
 	// Received Message
