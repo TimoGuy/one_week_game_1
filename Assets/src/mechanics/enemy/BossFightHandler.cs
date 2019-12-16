5ba1a7fbf43c32b75cf317f3612dea3dc84c9159
Hawsoo <Hawsoo@users.noreply.github.com>
2019-12-15 16:20:12 -0800

    POC: lazer follows player. Hmmm

--- Assets/src/mechanics/enemy/BossFightHandler.cs
+++ Assets/src/mechanics/enemy/BossFightHandler.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class BossFightHandler : MonoBehaviour {
 	public GameObject phase2;
 	public BossFightBubbleScores bubbleScores;
+	public Transform progLazerContainer;
+	public Transform playerCollision;
 	private Animator animator;
 
 	void Start () {
@@ -19,8 +21,33 @@ public class BossFightHandler : MonoBehaviour {
 		bubbleScores.SendMessage("StompedReceiveShock");
 	}
 
+	void Update () {
+		RandomChooseToDoLazerOrStomp();
+	}
+
 	void RandomChooseToDoLazerOrStomp () {
-		animator.SetBool("DoLazerBeam", Random.value > 0.5f);
+		if (!animator.GetBool("Override_DoLazerBeam")) {
+			animator.SetBool("DoLazerBeam", Random.value > 0.5f);
+		}
+	}
+
+	void CheckLazerTurnLeftOrRight () {
+		float playerAngFromOrigin = Mathf.Atan2(playerCollision.position.z, playerCollision.position.x) * Mathf.Rad2Deg;
+		float diff = Mathf.DeltaAngle(
+			-playerAngFromOrigin, progLazerContainer.rotation.eulerAngles.y
+		);
+		Debug.Log(diff);
+		if (diff < 0) {
+			animator.SetFloat("LazerSpinRight", 0);
+			var ang = progLazerContainer.rotation.eulerAngles;
+			ang.y += 45;
+			progLazerContainer.rotation = Quaternion.Euler(ang);
+		} else {
+			animator.SetFloat("LazerSpinRight", 1);
+			var ang = progLazerContainer.rotation.eulerAngles;
+			ang.y -= 45;
+			progLazerContainer.rotation = Quaternion.Euler(ang);
+		}
 	}
 
 	// Received Message
