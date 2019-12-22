f78124b8bf6ccd2a38843faa3315b37b26e0dd6c
Hawsoo <Hawsoo@users.noreply.github.com>
2019-12-21 19:12:05 -0800

    Adds a cutscene manager with some cutscenes
    Specifically the phase 2 and the dying cutscenes

---
+++ Assets/src/animations/CutsceneManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CutsceneManager : MonoBehaviour {
+	// void Start () {
+	// 	GetComponents<Camera>(cameras);
+	// 	Camera myCam = GetComponent<Camera>();
+	// 	cameras.Remove(myCam);
+	// }
+	private GameObject playerToReenable;
+	private Camera[] camsToReenable;
+
+
+	void OnEnable () {
+		camsToReenable = Camera.allCameras;
+		playerToReenable = GameObject.FindGameObjectWithTag("Player");
+
+		for (int i = 0; i < camsToReenable.Length; i++) {
+			camsToReenable[i].enabled = false;
+		}
+		playerToReenable.SetActive(false);
+
+		GetComponent<Camera>().enabled = true;
+	}
+
+	void OnDisable () {
+		for (int i = 0; i < camsToReenable.Length; i++) {
+			camsToReenable[i].enabled = true;
+		}
+		playerToReenable.SetActive(true);
+
+		GetComponent<Camera>().enabled = false;
+	}
+
+	void ShowTextbox () {
+
+	}
+}
