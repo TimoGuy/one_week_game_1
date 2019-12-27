6c67b5b76b77739287fa04c9ef3a001b0888eafd
Hawsoo <Hawsoo@users.noreply.github.com>
2019-12-26 23:00:35 -0800

    Creates intro spider cutscene

---
+++ Assets/src/animations/boss_attacks/SpiderAnimatorHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderAnimatorHandler : MonoBehaviour {
+	public Animator spiderAnimator;
+
+	void Start () {
+#if UNITY_EDITOR
+		if (spiderAnimator == null) {
+			Debug.LogError("spiderAnimator assignment is required");
+			UnityEditor.EditorApplication.isPlaying = false;
+		}
+#endif
+	}
+	
+	void Spider_EndIntroHold () { spiderAnimator.SetTrigger("End_Intro_Hold"); }
+	void Spider_JumpUp () { spiderAnimator.SetTrigger("Start_Jump_Up"); }
+	void Spider_ButtSlam () { spiderAnimator.SetTrigger("Start_Butt_Slam"); }
+	void Spider_DoLazer (bool flag) { spiderAnimator.SetBool("Do_Lazer", flag); }
+	void Spider_EndConcentrate () { spiderAnimator.SetTrigger("End_Concentrate"); }
+}
