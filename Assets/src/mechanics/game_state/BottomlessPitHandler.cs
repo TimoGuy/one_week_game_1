7488b39a1800b5a142ac1ce514d75ca06279571d
Hawsoo <Hawsoo@users.noreply.github.com>
2019-12-28 14:49:02 -0800

    Adds an effect that camera stops following player if bottomless pit

---
+++ Assets/src/mechanics/game_state/BottomlessPitHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BottomlessPitHandler : MonoBehaviour {
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Bottomless Pit") {
+			Camera.main.GetComponent<CameraInput>().enabled = false;
+		}
+	}
+}
