7ce05c37de1c7c2238348ec89b907f9b6cab80df
Hawsoo <Hawsoo@users.noreply.github.com>
2020-01-08 00:33:20 -0800

    Goto sleep cutscene

---
+++ Assets/src/mechanics/cutscenes/CutsceneGotoSleepHelper.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneGotoSleepHelper : MonoBehaviour {
+	public GameObject playerGameObj;
+	public Animator myCharPuppetAnimator;
+	public GSFirstArea gameStateManager;
+
+	void Start () {
+#if UNITY_EDITOR
+		if (playerGameObj == null) {
+			Debug.LogError("playerGameObj must not be null");
+			UnityEditor.EditorApplication.isPlaying = false;
+			return;
+		}
+		if (myCharPuppetAnimator == null) {
+			Debug.LogError("myCharPuppetAnimator must not be null");
+			UnityEditor.EditorApplication.isPlaying = false;
+			return;
+		}
+		if (gameStateManager == null) {
+			Debug.LogError("gameStateManager must not be null");
+			UnityEditor.EditorApplication.isPlaying = false;
+			return;
+		}
+#endif
+	}
+
+	void OnEnable () {
+		playerGameObj.SetActive(false);
+	}
+
+	void OnDisable () {
+		playerGameObj.SetActive(true);
+	}
+
+	public void TriggerPlayerLiedown () {
+		myCharPuppetAnimator.SetTrigger("Lie_Down");
+	}
+
+	public void MoveToGameState (int index) {
+		gameStateManager.gameStateIndex = index;
+	}
+}
