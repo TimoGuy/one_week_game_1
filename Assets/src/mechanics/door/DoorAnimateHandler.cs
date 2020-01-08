ccf676b0cf6dc528bd7bbd3b03978d195be9b9da
Hawsoo <Hawsoo@users.noreply.github.com>
2020-01-07 21:11:49 -0800

    Creates doors (unlockable and locked)

---
+++ Assets/src/mechanics/door/DoorAnimateHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorAnimateHandler : MonoBehaviour {
+	public Animator doorAnimator;
+
+	void Start () {
+#if UNITY_EDITOR
+		if (doorAnimator == null) {
+			Debug.LogError("doorAnimator assignment is required");
+			UnityEditor.EditorApplication.isPlaying = false;
+		}
+		if (gameObject.tag != "NPC Action") {
+			Debug.LogError("Action Area's gameObject.tag must be \"NPC Action\"");
+			UnityEditor.EditorApplication.isPlaying = false;
+		}
+#endif
+	}
+
+	void NPC_Action () {
+		doorAnimator.SetTrigger("Open_Door");
+		Destroy(gameObject);
+	}
+}
