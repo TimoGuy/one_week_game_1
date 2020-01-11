583e267a312814b65fc12531534868f4f529386e
Hawsoo <Hawsoo@users.noreply.github.com>
2020-01-10 18:38:07 -0800

    Starts the fall down shrine cutscene

---
+++ Assets/src/mechanics/cutscenes/CutsceneFallDownShrineHelper.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneFallDownShrineHelper : MonoBehaviour {
+	public GameObject playerGameObj;
+	public Animator gateAnimator;
+	private Animator myAnimator;
+
+	void Start () {
+#if UNITY_EDITOR
+		if (playerGameObj == null) {
+			Debug.LogError("playerGameObj must not be null");
+			UnityEditor.EditorApplication.isPlaying = false;
+			return;
+		}
+		if (gateAnimator == null) {
+			Debug.LogError("gateAnimator must not be null");
+			UnityEditor.EditorApplication.isPlaying = false;
+			return;
+		}
+#endif
+		myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnEnable () {
+		playerGameObj.SetActive(false);
+	}
+
+	void OnDisable () {
+		playerGameObj.SetActive(true);
+	}
+
+	public void OpenGate () {
+		gateAnimator.SetTrigger("Open_Gate");
+	}
+}
