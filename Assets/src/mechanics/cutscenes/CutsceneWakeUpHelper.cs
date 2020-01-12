526e956bca0609a8f0400c9afbb43271e9bcb9bc
Hawsoo <Hawsoo@users.noreply.github.com>
2020-01-11 23:07:04 -0800

    Adds tutorial tooltips and actual key for shrine

---
+++ Assets/src/mechanics/cutscenes/CutsceneWakeUpHelper.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneWakeUpHelper : MonoBehaviour {
+	public GameObject playerGameObj;
+	public GameObject myContainer;
+	public TextboxHandler handlerToWatch;
+	private Animator myAnimator;
+
+	void Start () {
+#if UNITY_EDITOR
+		if (playerGameObj == null) {
+			Debug.LogError("playerGameObj must not be null");
+			UnityEditor.EditorApplication.isPlaying = false;
+			return;
+		}
+		if (myContainer == null) {
+			Debug.LogError("myContainer must not be null");
+			UnityEditor.EditorApplication.isPlaying = false;
+			return;
+		}
+		if (handlerToWatch == null) {
+			Debug.LogError("handlerToWatch must not be null");
+			UnityEditor.EditorApplication.isPlaying = false;
+			return;
+		}
+#endif
+		myAnimator = GetComponent<Animator>();
+	}
+
+	private bool watchForTxtbxEnd = false;
+	void FixedUpdate () {
+		if (!watchForTxtbxEnd) return;
+
+		if (!handlerToWatch.enabled) {
+			// Turned off!
+			watchForTxtbxEnd = false;
+			myAnimator.SetTrigger("Do_Outro");
+		}
+	}
+
+	void OnEnable () {
+		playerGameObj.SetActive(false);
+	}
+
+	public void ReenablePlayer () {
+		playerGameObj.SetActive(true);
+	}
+
+	public void LockCursor () {
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	public void StartWatchingForTextboxEnd () {
+		watchForTxtbxEnd = true;
+	}
+
+	public void ShutDownCutscene () {
+		GameObject.Destroy(myContainer);
+	}
+}
--- Assets/src/mechanics/npc_action/ToolTip.cs
+++ Assets/src/mechanics/npc_action/ToolTip.cs
@@ -4,5 +4,5 @@ using System.Collections;
 public class ToolTip : MonoBehaviour {
 	public string tooltipText = "Press E to --";
 
-	public string GetText () { return tooltipText; }
+	public string GetText () { return tooltipText.Replace("\\n", "\n"); }
 }
