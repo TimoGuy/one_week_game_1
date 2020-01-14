2d771e413d6db47d0d8d7e4ced66177a80cb729b
Hawsoo <Hawsoo@users.noreply.github.com>
2020-01-13 19:25:05 -0800

    Edits house to be able to drop in. Adds thorny vines

---
+++ Assets/src/mechanics/enemy/BreakFromAttack.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakFromAttack : MonoBehaviour {
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag == "Player Weapon") {
+			Destroy(gameObject);
+		}
+	}
+}
