8d1c1b1aab60447d981295a1fcd18f42181fe2da
Hawsoo <Hawsoo@users.noreply.github.com>
2020-01-16 08:55:18 -0800

    Adds to die cutscene of spider to include dialogue from guardian

--- Assets/src/mechanics/cutscenes/CutsceneBossDieHelper.cs
+++ Assets/src/mechanics/cutscenes/CutsceneBossDieHelper.cs
@@ -5,10 +5,42 @@ using UnityEngine.SceneManagement;
 public class CutsceneBossDieHelper : MonoBehaviour {
 	public GameObject playerContainer;
 	public GameObject bossContainer;
+	public TextboxHandler handlerToWatch;
 
 	void Start () {
+#if UNITY_EDITOR
+		if (playerContainer == null) {
+			Debug.LogError("playerContainer must not be null");
+			UnityEditor.EditorApplication.isPlaying = false;
+			return;
+		}
+		if (bossContainer == null) {
+			Debug.LogError("bossContainer must not be null");
+			UnityEditor.EditorApplication.isPlaying = false;
+			return;
+		}
+		if (handlerToWatch == null) {
+			Debug.LogError("handlerToWatch must not be null");
+			UnityEditor.EditorApplication.isPlaying = false;
+			return;
+		}
+#endif
+
 		playerContainer.SetActive(false);
 		bossContainer.SetActive(false);
+	}
+
+	private bool watchForTxtbxEnd = false;
+	void FixedUpdate () {
+		if (!watchForTxtbxEnd) return;
+
+		if (!handlerToWatch.enabled) {
+			BossDieEvent();
+		}
+	}
+
+	public void StartWatchingForTextboxEnd () {
+		watchForTxtbxEnd = true;
 	}
 
 	void BossDieEvent () {
